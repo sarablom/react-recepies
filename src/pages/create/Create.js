35e021cf52ba15b1d474183c9f5f196bc906e54e
sarablom <sara.cecilia.blom@gmail.com>
2022-02-26 07:56:22 +0100

    add ingredients

--- src/pages/create/Create.js
+++ src/pages/create/Create.js
@@ -1,14 +1,29 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import "./Create.css";
 
 export default function Create() {
   const [title, setTitle] = useState("");
   const [method, setMethod] = useState("");
   const [cookingTime, setCookingTime] = useState("");
+  const [newIngredient, setNewIngredient] = useState("");
+  const [ingredients, setIngredients] = useState([]);
+  const ingredientInput = useRef(null);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(title, method, cookingTime);
+    console.log(title, method, cookingTime, ingredients);
+  }
+
+  const handleAdd = (e) => {
+    e.preventDefault();
+    const ing = newIngredient.trim();
+
+    if (ing && !ingredients.includes(ing)) {
+      setIngredients(prevIngredients => [prevIngredients, ing])
+    }
+    setNewIngredient("");
+    //focuses the input field so the user doesn't have to click
+    ingredientInput.current.focus();
   }
 
   return (
@@ -25,6 +40,20 @@ export default function Create() {
             required
           />
         </label>
+
+        <label>
+        <span>Recipe ingredients:</span>
+        <div className="ingredients">
+          <input 
+            type="text" 
+            onChange={(e) => setNewIngredient(e.target.value)}
+            value={newIngredient}
+            ref={ingredientInput}
+          />
+          <button className="btn" onClick={(e) => handleAdd(e)}>add</button>
+        </div>
+        </label>
+        <p>Current ingredients: {ingredients.map(i => <em key={i}>{i}, </em>)}</p>
 
         <label>
           <span>Recipe method:</span>
