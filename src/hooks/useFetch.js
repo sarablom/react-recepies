631a4402e07ad40ce711542b2ab5dd36e04b236a
sarablom <sara.cecilia.blom@gmail.com>
2022-02-27 15:13:45 +0100

    add POST request to useFetch

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -3,25 +3,36 @@
 //Shaun Pelling Udemy course React-Firebase
 import { useState, useEffect } from "react";
 
-export const useFetch = (url) => {
+export const useFetch = (url, method = "GET") => {
   const [data, setData] = useState(null);
   const [isPending, setIsPending] = useState(false);
   const [error, setError] = useState(null);
+  const [options, setOptions] = useState(null);
+
+  const postData = (postData) => {
+    setOptions({
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(postData),
+    });
+  };
 
   //use useRef to wrap an object/array argument
   //which is a useEffect dependency
   //const options = useRef(_options).current;
 
   useEffect(() => {
-      const controller = new AbortController();
+    const controller = new AbortController();
 
-    const fetchData = async () => {
+    const fetchData = async (fetchOptions) => {
       setIsPending(true);
 
       try {
-        const res = await fetch(url, { signal: controller.signal });
+        const res = await fetch(url, { ...fetchOptions,  signal: controller.signal });
         if (!res.ok) {
-            throw new Error(res.statusText);
+          throw new Error(res.statusText);
         }
         const json = await res.json();
 
@@ -29,22 +40,27 @@ export const useFetch = (url) => {
         setData(json);
         setError(null);
       } catch (err) {
-          if (err.name === "AbortError") {
-              console.log("The fetch was aborted");
-          } else {
-              setError("Could not fetch the data");
-              setIsPending(false);
-          }
+        if (err.name === "AbortError") {
+          console.log("The fetch was aborted");
+        } else {
+          setError("Could not fetch the data");
+          setIsPending(false);
+        }
       }
     };
 
-    fetchData();
+    if (method === "GET") {
+      fetchData();
+    }
+    if (method === "POST" && options) {
+      fetchData(options);
+    }
 
     //Clean up function
     return () => {
-        controller.abort();
-    }
-  }, [url]);
+      controller.abort();
+    };
+  }, [url, options, method]);
 
-  return { data, isPending, error };
+  return { data, isPending, error, postData };
 };
